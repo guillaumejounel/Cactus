43666a1a1dc926416cd53fd9aa9cd76d74220f79
jjerphan <julien.jerphanion@gmail.com>
2016-12-04 16:29:43 +0100

    Ajout liste objets utilisés

--- faits.lisp
+++ faits.lisp
@@ -2,3 +2,11 @@
 	(Propositions EQ nil)
 	(Application EQ nil)
 ))
+; Objets utilisés :
+; Application
+; Propositions
+; Precisions
+; Cible
+; Machine
+; Budget : numérique (pour utiliser > <)
+; Experience : numérique (pour utiliser > <)
