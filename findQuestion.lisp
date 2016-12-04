e53e68c3e732f41e00b99538e24ed6bf64cb01e2
Guillaume Jounel <guillaume.jounel@etu.utc.fr>
2016-12-04 16:43:11 +0100

    Wrote the algorithm

---
+++ findQuestion.lisp
@@ -0,0 +1,17 @@
+(defvar *attributs* nil)
+
+(defun listeAttributs ()
+
+)
+
+(defun findQuestion ()
+  ;lister les attributs de la base de faits
+  ;lister les attributs de la base de regles
+  ;faire la différence entre les deux listes : ce sont les attributs qui restent inconnus
+  ;si la liste résultante est vide, échec, aucun langage correspondant
+  ;sinon
+  ;  lister les valeurs possibles du premier attribut
+  ;  faire lire un choix à l'utilisateur (while (and (print valeurs) (read member of valeurs)))
+  ;  tant que son choix n'appartient pas à la liste d'attributs, redemander
+  ;  ajouter l'attribut / valeur à la base de regles
+)
