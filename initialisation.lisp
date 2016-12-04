5789832d025c747abc151b9341c9d959ad989145
Guillaume Jounel <guillaume.jounel@etu.utc.fr>
2016-12-04 18:58:32 +0100

    Added an example

--- initialisation.lisp
+++ initialisation.lisp
@@ -4,5 +4,9 @@
   (load (concatenate 'string path "fonctionsOutils.lisp"))
   (load (concatenate 'string path "faits.lisp"))
   (load (concatenate 'string path "moteur.lisp"))
+  (load (concatenate 'string path "askQuestion.lisp"))
   (ChainageAvantLarg)
 )
+
+;examples:
+;(init "~/Cloud/WORK/UTC/GI01/IA01/Cactus/")
